2b80cbd2bfca8fc65d75b4dddb0c31a2c74739d6
Miłosz Meller <git@milosz.me>
2025-03-15 12:31:52 +0100

    Finish Stage 5

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -6,24 +6,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        char[] input = new char[9];
+        Arrays.fill(input, '_');
         Main.printBoard(input);
-        input = Main.getMove(scanner, input);
-        Main.printBoard(input);
-        // System.out.println(gameStatus(input));
+        for (char symbol : "XOXOXOXOX".toCharArray()) {
+            int index = Main.getMove(scanner, input);
+            input[index] = symbol;
+            Main.printBoard(input);
+            String gameStatus = Main.gameStatus(input);
+            if (!gameStatus.equals("Game not finished")) {
+                System.out.println(gameStatus);
+                break;
+            }
+        }
     }
 
-    private static void printBoard(String input) {
+    private static void printBoard(char[] input) {
         System.out.println("---------");
-        System.out.printf("| %c %c %c |%n", input.charAt(0), input.charAt(1), input.charAt(2));
-        System.out.printf("| %c %c %c |%n", input.charAt(3), input.charAt(4), input.charAt(5));
-        System.out.printf("| %c %c %c |%n", input.charAt(6), input.charAt(7), input.charAt(8));
+        System.out.printf("| %c %c %c |%n", input[0], input[1], input[2]);
+        System.out.printf("| %c %c %c |%n", input[3], input[4], input[5]);
+        System.out.printf("| %c %c %c |%n", input[6], input[7], input[8]);
         System.out.println("---------");
     }
 
-    private static String getMove(Scanner scanner, String input) {
+    private static int getMove(Scanner scanner, char[] input) {
         int index;
-        char[] chars = input.toCharArray();
         while (true) {
             String move = scanner.nextLine();
             char[] moveChars = move.toCharArray();
@@ -41,23 +48,24 @@ public class Main {
                 continue;
             }
             index = x * 3 + y;
-            if (chars[index] != '_') {
+            if (input[index] != '_') {
                 System.out.println("This cell is occupied!");
                 continue;
             } else {
                 break;
             }
         }
-        chars[index] = 'X';
-        return new String(chars);
+        return index;
     }
 
-    private static String gameStatus(String input) {
+    private static String gameStatus(char[] input) {
+        /*
         long xs = input.chars().filter(c -> c == 'X').count();
         long os = input.chars().filter(c -> c == 'O').count();
         if (Math.abs(xs - os) > 1) {
             return "Impossible";
         }
+         */
 
         int[][] lineIndices = {
                 // rows
@@ -74,7 +82,7 @@ public class Main {
         };
         String[] lines = Arrays
                 .stream(lineIndices)
-                .map(line -> Main.lineFromIndices(input, line))
+                .map(line -> String.format("%c%c%c", input[line[0]], input[line[1]], input[line[2]]))
                 .toArray(String[]::new);
         long xWins = Arrays.stream(lines).filter(line -> line.equals("XXX")).count();
         long oWins = Arrays.stream(lines).filter(line -> line.equals("OOO")).count();
@@ -83,17 +91,17 @@ public class Main {
         } else if (xWins == 0 && oWins == 1) {
             return "O wins";
         } else if (xWins == 0 && oWins == 0) {
-            if (input.contains("_")) {
+            if (new String(input).contains("_")) {
                 return "Game not finished";
             } else {
                 return "Draw";
             }
-        } else {
+        }
+        /*
+        else {
             return "Impossible";
         }
-    }
-
-    private static String lineFromIndices(String input, int[] indices) {
-        return String.format("%c%c%c", input.charAt(indices[0]), input.charAt(indices[1]), input.charAt(indices[2]));
+         */
+        return "";
     }
 }
