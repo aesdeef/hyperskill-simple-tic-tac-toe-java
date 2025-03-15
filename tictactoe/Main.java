e232eada5edc4de34ebd028af898461a55c7d7ac
Miłosz Meller <git@milosz.me>
2025-03-15 09:41:16 +0100

    Finish Stage 2

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -1,9 +1,15 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("---------");
+        System.out.printf("| %c %c %c |%n", input.charAt(0), input.charAt(1), input.charAt(2));
+        System.out.printf("| %c %c %c |%n", input.charAt(3), input.charAt(4), input.charAt(5));
+        System.out.printf("| %c %c %c |%n", input.charAt(6), input.charAt(7), input.charAt(8));
+        System.out.println("---------");
     }
 }
